d5863fc07cc020d46f13ba2813856d2b7d6e00af
MurilloSSJ <murillostore@gmail.com>
2021-07-09 17:21:59 -0300

    Validação do formulario de cartegorias

--- routes/admin.js
+++ routes/admin.js
@@ -17,17 +17,45 @@ router.get('/newcartegory',(req,res)=>{
     res.render('admin/addcartegoria')
 })
 router.post('/addpost',(req,res) =>{
+    //Validando Formulario
     var erros = []
-
-
-    const novaCartegoria = {
-        nome: req.body.nomeCartegoria,
-        slug: req.body.slugCartegoria
+    var nomeCartegoria = req.body.nomeCartegoria
+    var slugCartegoria = req.body.slugCartegoria
+    if(!nomeCartegoria){
+        erros.push({
+            texto: "Nome Inválido"
+        })
     }
-    new model(novaCartegoria).save().then(()=>{
-        console.log("Salvo com sucesso!")
-    })
-    res.redirect('/admin')
+    if(!slugCartegoria){
+        erros.push({
+            texto:"Slug Inválido"
+        })
+    }
+    if(slugCartegoria.trim){
+        erros.push({
+            texto:"Slug não pode ter espaço, utilize o '-'"
+        })
+    }
+    if(slugCartegoria.isUpperCase){
+        erros.push({
+            texto:"Slug não pode ter letra maiuscula"
+        })
+    }
+    if(erros.length >0){
+        res.render('admin/addcartegoria',{erros: erros})
+    }
+    //Pegando os dados do formulario e inserindo no banco de dados
+    else{
+        const novaCartegoria = {
+            nome: req.body.nomeCartegoria,
+            slug: req.body.slugCartegoria
+        }
+        new model(novaCartegoria).save().then(()=>{
+            console.log("Salvo com sucesso!")
+        })
+        res.redirect('/admin')
+    }
+
 })
 // Exportando as rotas
 module.exports = router
