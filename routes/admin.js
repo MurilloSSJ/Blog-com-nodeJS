08ca604668767b53e9207df0c06ef55a8e873e74
MurilloSSJ <murillostore@gmail.com>
2021-07-10 17:17:09 -0300

    CRUD cartegorias

--- routes/admin.js
+++ routes/admin.js
@@ -1,32 +1,5 @@
-//Importando o Express
-const express = require("express")
-const router = express.Router()
-const mongoose = require('mongoose')
-require('../models/Cartegoria')
-const Cartegoria = mongoose.model("Cartegorias")
-//Definindo as rotas
-router.get('/',(req,res)=>{
-    res.render("admin/index")
-})
-router.get('/posts',(req,res)=>{
-    res.send("Página de posts")
-})
-router.get('/cartegorias',(req,res)=>{
-    Cartegoria.find().sort({Data:'desc'}).lean().then((cartegorias) =>{
-        console.log("Acessado com sucesso")
-        res.render('admin/cartegorias',{cartegorias : cartegorias})
-    }).catch((err)=>{
-        req.flash("errorMSG",err)
-    })
-})
-router.get('/newcartegory',(req,res)=>{
-    res.render('admin/addcartegoria')
-})
-router.post('/addpost',(req,res) =>{
-    //Validando Formulario
+function validandoFormularioCartegoria(nomeCartegoria,slugCartegoria){
     var erros = []
-    var nomeCartegoria = req.body.nomeCartegoria
-    var slugCartegoria = req.body.slugCartegoria
     if(!nomeCartegoria){
         erros.push({
             texto: "Nome Inválido"
@@ -37,13 +10,52 @@ router.post('/addpost',(req,res) =>{
             texto:"Slug Inválido"
         })
     }
+    var j=0
     for(let i=0;i<slugCartegoria.length;i++){
-        if(slugCartegoria[i] == ' '){
+        if(slugCartegoria[i] == ' ' && j==0){
+            j = 1
             erros.push({
                 texto:"Slug não pode ter espaço, utilize o '-'"
             })
         }
     }
+    return erros;
+}
+
+//Importando o Express
+const express = require("express")
+const router = express.Router()
+const mongoose = require('mongoose')
+require('../models/Cartegoria')
+const Cartegoria = mongoose.model("Cartegorias")
+//Definindo as rotas
+router.get('/',(req,res)=>{
+    res.render("admin/index")
+})
+
+router.get('/posts',(req,res)=>{
+    res.send("Página de posts")
+})
+
+router.get('/cartegorias',(req,res)=>{
+    Cartegoria.find().sort({Data:'desc'}).lean().then((cartegorias) =>{
+        res.render('admin/cartegorias',{cartegorias : cartegorias})
+    }).catch((err)=>{
+        req.flash("errorMSG",err)
+    })
+})
+
+router.get('/newcartegory',(req,res)=>{
+    res.render('admin/addcartegoria')
+})
+
+//Adicionando, removendo e alterando cartegorias
+    //Adicionando Nova Cartegoria
+router.post('/addpost',(req,res) =>{
+    //Validando Formulario
+    var nomeCartegoria = req.body.nomeCartegoria
+    var slugCartegoria = req.body.slugCartegoria
+    var erros = validandoFormularioCartegoria(nomeCartegoria,slugCartegoria)
     if(erros.length >0){
         res.render('admin/addcartegoria',{erros: erros})
     }
@@ -61,6 +73,51 @@ router.post('/addpost',(req,res) =>{
             res.redirect('/admin/cartegorias')
         })
     }
+})
+//Editando Cartegoria
+router.get('/cartegorias/edit/:id',(req,res)=>{
+    Cartegoria.findOne({_id:req.params.id}).lean().then((cartegoria)=>{
+        res.render('admin/editarCartegoria',{cartegoria:cartegoria})
+    }).catch((err)=>{
+        req.flash("errorMSG","Esta cartegoria não existe")
+        res.redirect('/admin/cartegorias')
+    })
+})
+router.post('/cartegorias/editsuccess',(req,res)=>{
+    var nomeCartegoria = req.body.nomeCartegoria
+    var slugCartegoria = req.body.slugCartegoria
+    var erros = validandoFormularioCartegoria(nomeCartegoria,slugCartegoria)
+    if(erros.length >0){
+        res.render('admin/editarCartegoria',{erros: erros})
+    }
+    else{
+        Cartegoria.findOne({_id: req.body.idCartegoria}).then((editarcartegoria)=>{
+            editarcartegoria.nome = req.body.nomeCartegoria
+            editarcartegoria.slug = req.body.slugCartegoria
+            editarcartegoria.save().then(()=>{
+                console.log("Entrou2")
+                req.flash('successMSG','Cartegoria alterada com sucesso!')
+                res.redirect('/admin/cartegorias')
+            }).catch((err)=>{
+                req.flash('errorMSG','Ops! infelizmente ocorreu o erro: '+err)
+                res.redirect('/admin/cartegorias')
+            })
+        }).catch((err)=>{
+            req.flash('errorMSG',"ID não encontrado "+ err)
+            res.redirect('/admin/cartegorias')
+        })
+    }
+})
+
+//Deletando Cartegorias
+router.get('/cartegorias/remove/:id',(req,res)=>{
+    Cartegoria.remove({_id: req.params.id}).then(()=>{
+        req.flash('successMSG','Arquivo removido com sucesso')
+        res.redirect('/admin/cartegorias')
+    }).catch((err)=>{
+        req.flash('errorMSG','Falha ao remover, erro: '+err)
+        res.redirect('/admin/cartegorias')
+    })
 })
 // Exportando as rotas
 module.exports = router
