feae79597c058ce61cf3fb6e5cb7ff2bffdc1553
MurilloSSJ <murillostore@gmail.com>
2021-07-10 22:56:20 -0300

    Model de posts

---
+++ models/Posts.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Post = new Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    content:{
+        type: String,
+        required: true
+    }
+})
