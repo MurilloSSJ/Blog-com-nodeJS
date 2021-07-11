de8afc27d78fd1112edd1fbe507578b5c9f57294
MurilloSSJ <murillostore@gmail.com>
2021-07-11 18:45:13 -0300

    Listagem de posts & homepage

--- routes/public.js
+++ routes/public.js
@@ -6,12 +6,19 @@ const posts = mongoose.model('postagens')
 
 //Definindo rotas
 PublicRoutes.get('/',(req,res)=>{
-    posts.find().lean().then((posts)=>{
+    posts.find().sort({date:'desc'}).populate('cartegory').lean().then((posts)=>{
         res.render("User/index",{post:posts})
 })
     })
-PublicRoutes.get('/posts',(req,res)=>{
-    res.send("Lista de posts")
+PublicRoutes.get('/posts/:slug',(req,res)=>{
+    posts.findOne({slug:req.params.slug}).populate('cartegory').lean().then((postagem)=>{
+        if(postagem){
+            res.render('User/posts/listaposts',{post:postagem})
+        }else{
+            req.flash('errorMSG','Postagem não encontrada!')
+            res.redirect('/')
+        }
+    })
 })
 
 module.exports = PublicRoutes
