6dfd1a07ec13a49d8e26d47d1e760cb58a8f4838
MurilloSSJ <murillostore@gmail.com>
2021-07-12 01:18:45 -0300

    Atualização Porta

--- app.js
+++ app.js
@@ -13,7 +13,7 @@
     const passport = require("passport")
     require('./config/auth')(passport)
 //Constantes do Programa
-    const port = process.env.port || 3000
+    const port = process.env.PORT || 3000
     const db = require('./config/db')
 //Configurações
     //Sessão
